7555d696f251019b0e3ea7c717b7ff20781bc33e
Thanh-Tien Phan <tien4112004@gmail.com>
2025-12-14 16:18:03 +0700

    add imap routes

--- src/database/entities/index.ts
+++ src/database/entities/index.ts
@@ -3,5 +3,6 @@ import { Email } from './email.entity';
 import { Snooze } from './snooze.entity';
 import { KanbanColumn } from './kanban-column.entity';
 import { KanbanCard } from './kanban-card.entity';
+import { SmtpConfig } from './smtp-config.entity';
 
-export const entities = [User, Email, Snooze, KanbanColumn, KanbanCard];
+export const entities = [User, Email, Snooze, KanbanColumn, KanbanCard, SmtpConfig];
