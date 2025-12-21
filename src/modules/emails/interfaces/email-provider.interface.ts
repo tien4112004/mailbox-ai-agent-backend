74deff47be6de7c718af2db44d18f47b740cbede
Thanh-Tien Phan <tien4112004@gmail.com>
2025-12-21 09:30:52 +0700

    feat: add unique constraint on message_id to prevent duplicates

--- src/modules/emails/interfaces/email-provider.interface.ts
+++ src/modules/emails/interfaces/email-provider.interface.ts
@@ -1,6 +1,6 @@
 export interface EmailProvider {
   listMailboxes(): Promise<any[]>;
-  listEmails(folder: string, limit: number, pageToken?: string, search?: string): Promise<any>;
+  listEmails(folder: string, limit: number, pageToken?: string, search?: string, forceSync?: boolean): Promise<any>;
   getEmailById(emailId: string): Promise<any>;
   sendEmail(to: string[], subject: string, body: string, cc?: string[], bcc?: string[], inReplyTo?: string, references?: string): Promise<any>;
   modifyEmail(emailId: string, addLabelIds?: string[], removeLabelIds?: string[]): Promise<void>;
